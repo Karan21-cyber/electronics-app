19d6b796b6a11a79e117a15c89b567d9b74dbf8b
Karan Chaudhary <karan.chaudhary@aitc.ai>
2023-08-28 17:03:15 +0545

    update : todays work

--- app/components/reusable/review-card.tsx
+++ app/components/reusable/review-card.tsx
@@ -3,14 +3,14 @@ import React from "react";
 
 function ReviewCard() {
   return (
-    <div className="review-card-wrapper  w-full flex flex-col bg-[#F5F7FF] ">
+    <div className="review-card-wrapper  w-full flex flex-col bg-[#F5F7FF] py-4">
       <div className="review-detail-wrapper flex gap-4">
         <Image
           src={"/home/review_icon.png"}
           width={50}
           height={50}
           alt="review-image"
-          className="object-cover"
+          className="object-contain"
         />
         <h1 className="review-description text-lg font-normal">
           My first order arrived today in perfect condition. From the time I
@@ -21,7 +21,7 @@ function ReviewCard() {
         </h1>
       </div>
       <h1 className="review-name text-right text-sm font-normal">- John Doe</h1>
-      <button className="review-button text-sm w-[200px] font-semibold text-center px-6 py-2 text-[#0156FF] border-[#0156FF] border-2 rounded-[50px]">
+      <button className="review-button text-sm w-[200px] mt-10 font-semibold text-center px-6 py-2 text-[#0156FF] border-[#0156FF] border-2 rounded-[50px]">
         Leave Us A Review
       </button>
     </div>
